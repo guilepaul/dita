63d30bc3227cbb4c38f007427bc352d0abbf593f
Carlos Daniel <killer.dk20@gmail.com>
2021-09-21 23:31:29 -0300

    feat: modal

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  dita
+//
+//  Created by Carlos Daniel on 21/09/21.
+//
+
+import Foundation
--- src/components/Plan/components/ModalPlan/index.js
+++ src/components/Plan/components/ModalPlan/index.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import {View, Pressable, Text} from 'react-native';
 
 import * as S from './styles';
 
 export const ModalPlan = props => {
+  const handleCloseModal = () => {
+    props.setModalVisible(!props.modalVisible);
+  };
+
   return (
-    <View>
+    <S.Container>
       <S.ModalView
-        animationType="slide"
-        transparent={true}
+        animationType="fade"
+        transparent
         visible={props.modalVisible}
-        onRequestClose={() => {
-          props.setModalVisible(!props.modalVisible);
-        }}>
+        onRequestClose={handleCloseModal}>
         <S.CenteredView>
           <S.ViewModal>
-            <S.TextModal>Hello World!</S.TextModal>
-            <Pressable
-              onPress={() => props.setModalVisible(!props.modalVisible)}>
-              <Text>Hide Modal</Text>
-            </Pressable>
+            <S.Info color={props.color}>{props.text}</S.Info>
+            <S.InnerContainer>
+              <S.Paragraph>{props.innerText}</S.Paragraph>
+            </S.InnerContainer>
+            <S.Value>{props.value.replace('a partir de', '')}</S.Value>
+            <S.ButtonCloseModal color={props.color} onPress={handleCloseModal}>
+              <S.ButtonText>Fechar</S.ButtonText>
+            </S.ButtonCloseModal>
           </S.ViewModal>
         </S.CenteredView>
       </S.ModalView>
-    </View>
+    </S.Container>
   );
 };
